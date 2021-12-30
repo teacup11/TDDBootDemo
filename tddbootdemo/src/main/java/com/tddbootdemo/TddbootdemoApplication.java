a265f0d8acac968610579e4338d3713915fcaab0
teacup11 <ttea.kknezevic@gmail.com>
2021-12-30 19:25:20 +0100

    first commit. add spring boot project

---
+++ tddbootdemo/src/main/java/com/tddbootdemo/TddbootdemoApplication.java
@@ -0,0 +1,13 @@
+package com.tddbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddbootdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddbootdemoApplication.class, args);
+	}
+
+}
